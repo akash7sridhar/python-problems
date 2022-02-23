9cf384038f031e962cedb045c7486c6642604c23
akash7sridhar <akash7sridhar@gmail.com>
2022-02-24 01:20:01 +0530

    Add: even or odd problem

---
+++ even_or_odd.py
@@ -0,0 +1,13 @@
+def main():
+    # YOUR CODE GOES HERE
+    # Please take input and print output to standard input/output (stdin/stdout)
+    # E.g. 'input()/raw_input()' for input & 'print' for output
+    N = int(input())
+    if N % 2 == 0:
+        print(0)
+    else:
+        print(1)
+    return 0
+
+if __name__ == '__main__':
+    main()
