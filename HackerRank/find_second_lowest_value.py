89bae6402303822376872004d3d179bde787c1d2
akash7sridhar <akash7sridhar@gmail.com>
2022-02-26 04:02:16 +0530

    Add: new questions

--- HackerRank/find_second_lowest_value.py
+++ HackerRank/find_second_lowest_value.py
@@ -34,24 +34,19 @@ if __name__ == '__main__':
         score = float(input())
         students_list.append([name,score])
     students_list.sort(key = lambda x: x[1])
-    print(students_list)
     # Finds Second Lowest Mark
-    second_lowerst = []
+    second_lowerst = 0.00
 
     for i in range(N-1):
         if students_list[i][1] < students_list[i+1][1]:
-            second_lowerst.append(students_list[i][1])
-            print(students_list[i+1][1])
+            second_lowerst = students_list[i+1][1]
             break
-    print(second_lowerst)
-    second_lowerst_index = []
-    # Find index where second Lowest Mark is Present
-    
-    for i in range(N-1):
-        if students_list[1] == second_lowerst:
-            # print(students_list[i][0])
-            second_lowerst_index.append(students_list[i][0])
-    print(second_lowerst_index)
-    # Print Names of Second Lowest Mark
-    for i in second_lowerst_index:
-        print(students_list[i][0])
+    # Find Name of student where second Lowest Mark is Present
+    second_lowerst_name = []
+
+    for key,value in students_list:
+        if value == second_lowerst:
+            second_lowerst_name.append(key)
+    second_lowerst_name.sort()
+    for name in second_lowerst_name:
+      print(name)
---
+++ HackerRank/swap_case.py
@@ -0,0 +1,7 @@
+def swap_case(s):
+    lower_case = s.swapcase()
+    return lower_case
+
+if __name__ == '__main__':
+    string = input()
+    swap_case(string)
