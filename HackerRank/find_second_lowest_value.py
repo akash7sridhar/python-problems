6da69fdab15bfea01ad630feb69b11a383e8dcac
akash7sridhar <akash7sridhar@gmail.com>
2022-02-26 02:43:53 +0530

    Add: HackerRank question

--- HackerRank/find_second_lowest_value.py
+++ HackerRank/find_second_lowest_value.py
@@ -1,3 +1,30 @@
+# Given the names and grades for each student in a class of  students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+# Note: If there are multiple students with the second lowest grade, order their names alphabetically and print each name on a new line.
+# Example
+# The ordered list of scores is , so the second lowest score is . There are two students with that score: . Ordered alphabetically, the names are printed as:
+# alpha
+# beta
+
+# Output Format
+# Print the name(s) of any student(s) having the second lowest grade in. If there are multiple students, order their names alphabetically and print each one on a new line.
+# Sample Input 0
+
+# 5
+# Harry
+# 37.21
+# Berry
+# 37.21
+# Tina
+# 37.2
+# Akriti
+# 41
+# Harsh
+# 39
+# Sample Output 0
+
+# Berry
+# Harry
+
 if __name__ == '__main__':
     students_list = []
     N = int(input())
